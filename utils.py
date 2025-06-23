0cc7c5198dd5cd44549bd540f18b398b7372b7ac
Yairzeh <163775453+Yairzeh@users.noreply.github.com>
2025-06-23 20:00:07 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,9 +9,18 @@ def load_json_file(file_path):
         return json.load(file)
 
 
-def dump_to_json(file_path, data):
+def dump_to_json(file_path, data, indent=2):
     """
-    dumps the config file, with indent=2
+    dumps the config file, with indent.
     """
     with open(file_path, 'w') as file:
-        json.dump(data, file, indent=2)
+        json.dump(data, file, indent=indent)
+
+
+def load_and_dump_to_json(file_path, key, value, indent=2):
+    """
+    loads the config file, and dumps the key:value pair into it.
+    """
+    file = load_json_file(file_path)
+    file[key] = value
+    dump_to_json(file_path, file, indent)
