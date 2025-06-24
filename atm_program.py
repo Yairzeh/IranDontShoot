e858281b6e306a01336cdd7a14ad245161813ec7
Yairzeh <163775453+Yairzeh@users.noreply.github.com>
2025-06-24 21:44:01 +0300

    Update atm_program.py

--- atm_program.py
+++ atm_program.py
@@ -1,9 +1,10 @@
-from useraccounts import UserAccounts
+from user_accounts import UserAccounts
+from utils import exit_program
 
-CONFIG_FILE_PATH = 'config.json'
+ACCOUNTS_INFO_FILE_PATH = 'accounts_info.json'
 
 
-def get_startup_method(user_accounts, file_path):
+def get_startup_action(user_accounts, file_path):
     """
     Method factory for the startup menu choices.
     """
@@ -18,14 +19,13 @@ def get_startup_choice():
     """
     returns the user's startup menu input, after checking that it's a valid choice.
     """
-    choice = input('\nWelcome! Here are your options:\n\n'
+    choice = input('\nWelcome! Here are your options:\n'
                    '[1] Log-in\n'
                    '[2] Exit out of program\n'
                    '[3] Create a new account\n'
                    'Enter your choice: ')
     if choice not in ['1', '2', '3']:
-        print('Invalid choice!')
-        raise ValueError
+        raise ValueError('\nInvalid choice!')
     return choice
 
 
@@ -41,34 +41,35 @@ def get_user_action():
                    '[5] Save & Exit\n'
                    'Enter your choice: ')
     if choice not in ['1', '2', '3', '4', '5']:
-        print('Invalid choice!')
-        raise ValueError
+        raise ValueError('\nInvalid choice!')
     return choice
 
 
 def start_action_loop(user_accounts, file_path):
     """
     asks for the account number and password, if correct brings the user to the action menu and performs the action for
-    each input.
+    each input. if choice was 5 then saves the accounts to the file and breaks the loop.
     """
     print("\nLogging in...")
     try:
-        account = user_accounts.load_account()
-    except ValueError:
+        account = user_accounts.log_into_account()
+    except ValueError as e:
+        print(e)
         return
     while True:
         try:
             choice = get_user_action()
-        except ValueError:
+        except ValueError as e:
+            print(e)
             continue
-        account.get_action_method(user_accounts, file_path).get(choice)()
         if choice == '5':
+            print('\nSaving and exiting...')
+            user_accounts.dump_accounts_to_file(file_path)
             break
-
-
-def exit_program():
-    print('\nExiting program...')
-    exit()
+        try:
+            account.get_user_actions.get(choice)()
+        except ValueError as e:
+            print(e)
 
 
 def start_program_loop(user_accounts, file_path):
@@ -79,9 +80,13 @@ def start_program_loop(user_accounts, file_path):
         while True:
             try:
                 choice = get_startup_choice()
-            except ValueError:
+            except ValueError as e:
+                print(e)
                 continue
-            get_startup_method(user_accounts, file_path).get(choice)()
+            try:
+                get_startup_action(user_accounts, file_path).get(choice)()
+            except ValueError as e:
+                print(e)
 
 
-start_program_loop(UserAccounts(CONFIG_FILE_PATH), CONFIG_FILE_PATH)
+start_program_loop(UserAccounts.get_accounts_from_file(ACCOUNTS_INFO_FILE_PATH), ACCOUNTS_INFO_FILE_PATH)
